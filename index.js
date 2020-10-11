e8f4b54eaddcb8f5b941b44d6767435002ad578a
Clarissa Reeve <clarissa.reeve@gmail.com>
2020-10-11 17:59:27 -0600

    added ascii header

--- index.js
+++ index.js
@@ -20,6 +20,24 @@ connection.connect((err) => {
         throw err;
     }
     console.log('MySql Connected...');
+
+    console.log('   ___  ___ ___  ____   _       ___   __ __    ___    ___ ');
+    console.log('  \/  _]|   |   ||    \\ | |     \/   \\ |  |  |  \/  _]  \/  _]');
+    console.log(' \/  [_ | _   _ ||  o  )| |    |     ||  |  | \/  [_  \/  [_ ');
+    console.log('|    _]|  \\_\/  ||   _\/ | |___ |  O  ||  ~  ||    _]|    _]');
+    console.log('|   [_ |   |   ||  |   |     ||     ||___, ||   [_ |   [_ ');
+    console.log('|     ||   |   ||  |   |     ||     ||     ||     ||     |');
+    console.log('|_____||___|___||__|   |_____| \\___\/ |____\/ |_____||_____|');
+    console.log('                                                          ');
+    console.log('     ___ ___   ____  ____    ____   ____    ___  ____     ');
+    console.log('    |   |   | \/    ||    \\  \/    | \/    |  \/  _]|    \\    ');
+    console.log('    | _   _ ||  o  ||  _  ||  o  ||   __| \/  [_ |  D  )   ');
+    console.log('    |  \\_\/  ||     ||  |  ||     ||  |   |    _]|    \/    ');
+    console.log('    |   |   ||  _  ||  |  ||  _  ||  |_  |   [_ |    \\    ');
+    console.log('    |   |   ||  |  ||  |  ||  |  ||     ||     ||  .  \\   ');
+    console.log('    |___|___||__|__||__|__||__|__||___,_||_____||__|\\_|   ');
+
+    connection.end()
 });
 
 // ends the connection whenever anything closes the process
